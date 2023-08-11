55ee2794e08b751e2c6a94b2528cf1c0a9f4af1d
rahatcnits <rahatcnits@gmail.com>
2023-08-11 22:43:25 +0600

    user form modal dom works

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ dotenv.config();
 // Databaase Connection
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.MONGO_CONnECtion_STRING, {
+  .connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
